f1a85a91e756b23d1bfdff49a9d4a160215adca5
sajidkhan786666 <112601462+sajidkhan786666@users.noreply.github.com>
2022-09-27 20:38:28 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['Your Number']923497194834
+global.premium = ['Your Number']923497194834
+global.ownernomer = 'Your Number'923497194834
+global.ownername =xxx man
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
